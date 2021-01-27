99ebbe0c8649f7adc698c7c4c4f5f78e5d3ab6e8
Rakshitjn <rakshitjn2006@gmail.com>
2021-01-27 19:26:44 +0530

    New file

--- Food/views.py
+++ Food/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render,HttpResponse
 from .models import Item
+from django.template import loader
 # Create your views here.
 def index(request):
-    return HttpResponse(Item.objects.all())
+    item_list = Item.objects.all()
+    
+    context = {
+        'item_list':item_list,
+    }
+    return render(request,'food/index.html',context)
